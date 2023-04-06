e3c6acd133c1e7847b7275b57bfd77239bc278bc
Matcha204 <96802043+Matcha204@users.noreply.github.com>
2023-04-06 16:46:07 -0600

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Fetch the current date and update it in the DOM
+    $(document).ready(function () {
+        $("#display_date").html(display_date)
+    })
+
+
+
+
+    //  write an event, when Submit button is clicked
+    $('').click(function(){
+
+        //  get the text value from the textarea using the 'val()' method
+        let text_value = $('').val()
+
+        //  Convert it to JS object.
+        //  Provide a 'key' here and in write the same in app.py file as well to extract data
+        let input_text = {'' : text_value}
+        console.log(input_text)
+
+        //  ajax request
+        $.ajax({
+            type: 'POST',
+            url: "/predict-emotion",
+            data: JSON.stringify(input_data),
+            dataType: "json",
+            contentType: 'application/json',
+            success: function (result) {
+
+            //  type of web request
+                
+                // Result Received From Flask ----->JavaScript
+                predicted_emotion = result.data.predicted_emotion
+                emo_url = result.data.predicted_emotion_img_url
+
+                
+                // Display Result Using JavaScript----->HTML
+                $("#prediction").html(predicted_emotion)
+                $('#prediction').css("display", "block");
+
+                $("#emo_img_url").attr('src', emo_url);
+                $('#emo_img_url').css("display", "block");
+
+                // extract prediction and emoticon url from result
+                
+
+
+                //  update the DOM elements
+
+
+                //  show them
+
+            },
+
+            //  if any error, run this function
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  clearing the textbox after every button push
+        $('#text').val("")
+    })
+        
+})
